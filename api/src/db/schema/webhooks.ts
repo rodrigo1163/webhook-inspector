2a0f254c4490c61141be4a36d70ca07a322587f9
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-12-19 14:59:10 -0400

    feat: create api structure with docker, drizzle and fastify

---
+++ api/src/db/schema/webhooks.ts
@@ -0,0 +1,16 @@
+import { integer, text, jsonb, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { uuidv7 } from 'uuidv7'
+
+export const webhooks = pgTable('webhooks', {
+  id: text().primaryKey().$default(() => uuidv7()),
+  method: text().notNull(),
+  pathname: text().notNull(),
+  ip: text().notNull(),
+  statusCode: integer().notNull().default(200),
+  contentType: text(),
+  contentLength: integer(),
+  queryParams: jsonb().$type<Record<string, string>>(),
+  headers: jsonb().$type<Record<string, string>>().notNull(),
+  body: text(),
+  createAt: timestamp().notNull().defaultNow()
+})
---
+++ api/src/server.ts
@@ -0,0 +1,45 @@
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform, 
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhook'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector Api',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0',
+    }
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs'
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('🔥 HTTP server running on http://localhost:3333')
+  console.log('📚 Docs available at http://localhost:3333/docs')
+})
